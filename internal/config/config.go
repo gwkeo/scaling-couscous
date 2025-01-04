424720032d940707bd82493f9d84528c796afe50
Rakhman Imanov <raxman.i@mail.ru>
2025-01-04 21:39:02 +0300

    Написан обработчик конфиг файла

---
+++ internal/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"errors"
+	"gopkg.in/yaml.v3"
+	"io"
+	"os"
+)
+
+type Config struct {
+	DBPath string `yaml:"db_path"`
+}
+
+func Init() (*Config, error) {
+	cfgPath := os.Getenv("CONFIG_PATH")
+	if cfgPath == "" {
+		return nil, errors.New("CONFIG_PATH environment variable not set")
+	}
+
+	file, err := os.Open(cfgPath)
+	if err != nil {
+		return nil, errors.New("failed to open config file: " + err.Error())
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return nil, errors.New("failed to read config file: " + err.Error())
+	}
+
+	var cfg *Config
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, errors.New("unable to parse config: " + err.Error())
+	}
+
+	return cfg, nil
+}
