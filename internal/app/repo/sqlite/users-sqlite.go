e30f637f9403dff6194f595fd341875b0e827627
Rakhman Imanov <raxman.i@mail.ru>
2025-02-05 10:25:17 +0300

    Добавлен столбец с ролями пользователей

--- internal/app/repo/sqlite/users-sqlite.go
+++ internal/app/repo/sqlite/users-sqlite.go
@@ -23,7 +23,8 @@ func NewUsersRepo(ctx context.Context, path string) (*UsersRepoSqlite, error) {
 	CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		email VARCHAR(255) UNIQUE NOT NULL,
-	    password VARCHAR(255) NOT NULL);
+	    password VARCHAR(255) NOT NULL,
+		role VARCHAR(255) NOT NULL);
 	
 	`)
 	if err != nil {
